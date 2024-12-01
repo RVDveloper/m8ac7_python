ddcea73d60f01d28d379cf9063859b4b62806d8c
RVDveloper <yorjes.valerio@estudiant.fjaverianas.com>
2024-12-01 20:30:18 +0100

    tasca 4

--- m8ac7.py
+++ m8ac7.py
@@ -4,18 +4,18 @@ import mysql.connector
 
 app = Flask(__name__)
 
-# def registerConnection():
-#    db = mysql.connector.connect(user='usuari1', password='',
-#                               host='localhost',
-#                               database='m8ac7')
-#    cursor = db.cursor()
-#    cursor.execute("insert into prova() values();")
-#    db.commit()
-#    db.close()
+def registerConnection():
+   db = mysql.connector.connect(user='usuari1', password='',
+                              host='localhost',
+                              database='m8ac7')
+   cursor = db.cursor()
+   cursor.execute("insert into prova() values();")
+   db.commit()
+   db.close()
 
 @app.route('/') 
 def hello_world():
-   # registerConnection()
+   registerConnection()
    pyversion = sys.version_info[0]
    flaskversion = __version__
    return render_template("index.html",
